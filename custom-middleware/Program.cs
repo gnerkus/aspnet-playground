92018d8c5b5e8afc307ddddfe82c7bce6dbb8f6d
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-09-03 19:20:11 +0200

    define extension method for custom middleware

--- custom-middleware/Program.cs
+++ custom-middleware/Program.cs
@@ -1,4 +1,5 @@
 using System.Globalization;
+using custom_middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,20 +13,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.Use(async (context, next) =>
-{
-    var cultureQuery = context.Request.Query["culture"];
-    if (!string.IsNullOrWhiteSpace(cultureQuery))
-    {
-        var culture = new CultureInfo(cultureQuery);
-
-        CultureInfo.CurrentCulture = culture;
-        CultureInfo.CurrentUICulture = culture;
-    }
-
-    // Call the next delegate/middleware in the pipeline.
-    await next(context);
-});
+app.UseRequestCulture();
 
 app.Run(async (context) =>
 {
